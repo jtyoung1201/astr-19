42dbf26668970835a455e7f0b24a2fde8388fadb
jtyoung1201 <jatyoung@ucsc.edu>
2025-10-19 23:11:50 -0700

    Code Journal 2

---
+++ code2.py
@@ -0,0 +1,18 @@
+float1 = 4.2
+float2 = 6.9
+int1 = 10
+int2 = 4
+
+sum = float1 + float2
+difference = int1 - int2
+product = float1 * int2
+
+# Print results and their data types
+print("sum of floats:", sum_result)
+print("Data type of sum:", type(sum))
+
+print("Difference between two integers:", difference)
+print("Data type of difference:", type(difference))
+
+print("Product of a float and an integer:", product)
+print("Data type of product:", type(product))
